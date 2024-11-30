7caced5251a77c38fef475ff85b8addb69c9d83f
CostaKK <luquinhas.btr@gmail.com>
2024-11-30 17:14:52 -0300

    jogo de perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Quem é o amigo do homem, além do cachorro?',
+    'Cavalo'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que continente se encontra o país Sri Lanka?',
+    'Ásia.'
+)
+
+criaCartao(
+    'Ciências',
+    'Como é o nome do diagrama que representa a distribuição eletrônica através de subníveis de energia?',
+    'Diagrama de Pauling'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Qual a tradução da palavra pneumonoultramicroscopicsilicovolcanoconiosis?',
+    'pneumonoultramicroscópicosilicovulcanoconiose'
+)
